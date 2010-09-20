3092dc98b53fedae2d6001da019a740c5f5d28bf
Fuzail Sarang <fuzail@fingerprintsoft.org>
2010-09-20 12:31:10 +0200

    Updated License.

--- src/main/java/org/fingerprintsoft/io/IFileService.java
+++ src/main/java/org/fingerprintsoft/io/IFileService.java
@@ -1,3 +1,19 @@
+/*
+ * org/fingerprintsoft/io/IFileService.java
+ *
+ * Copyright (C) 2010 Fingerprints Software
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.fingerprintsoft.io;
 
 import java.util.Map;
@@ -10,10 +26,11 @@ import java.util.Map;
  * 
  * 
  * @author Fuzail Sarang <fuzail@fingerprintsoft.org>
+ * @link http://git://github.com/fingerprints/fileservlet.git
  * 
  */
 public interface IFileService {
 
-	String getFilePath(Map<String, Object> searchParamaters);
+    String getFilePath(Map<String, Object> searchParamaters);
 
 }
